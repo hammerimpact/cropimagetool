852e81fef5b5a7a0547edbfb549ca280b4aefe2f
HammerMan <hammerimpact88@gmail.com>
2020-09-23 22:19:11 +0900

    [System] Add FileCopy Tool

---
+++ ImageAnalyzerApp/FileMoveApp/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FileMoveApp
+{
+    partial class FileMoveForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSour = new System.Windows.Forms.TextBox();
+            this.textBoxDest = new System.Windows.Forms.TextBox();
+            this.buttonSour = new System.Windows.Forms.Button();
+            this.buttonDest = new System.Windows.Forms.Button();
+            this.buttonDo = new System.Windows.Forms.Button();
+            this.folderBrowserDialogForMove = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBoxNames = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxSour
+            // 
+            this.textBoxSour.Location = new System.Drawing.Point(12, 26);
+            this.textBoxSour.Name = "textBoxSour";
+            this.textBoxSour.ReadOnly = true;
+            this.textBoxSour.Size = new System.Drawing.Size(407, 21);
+            this.textBoxSour.TabIndex = 0;
+            // 
+            // textBoxDest
+            // 
+            this.textBoxDest.Location = new System.Drawing.Point(12, 72);
+            this.textBoxDest.Name = "textBoxDest";
+            this.textBoxDest.ReadOnly = true;
+            this.textBoxDest.Size = new System.Drawing.Size(407, 21);
+            this.textBoxDest.TabIndex = 1;
+            // 
+            // buttonSour
+            // 
+            this.buttonSour.Location = new System.Drawing.Point(441, 26);
+            this.buttonSour.Name = "buttonSour";
+            this.buttonSour.Size = new System.Drawing.Size(107, 21);
+            this.buttonSour.TabIndex = 2;
+            this.buttonSour.Text = "Sour";
+            this.buttonSour.UseVisualStyleBackColor = true;
+            this.buttonSour.Click += new System.EventHandler(this.buttonSour_Click);
+            // 
+            // buttonDest
+            // 
+            this.buttonDest.Location = new System.Drawing.Point(441, 72);
+            this.buttonDest.Name = "buttonDest";
+            this.buttonDest.Size = new System.Drawing.Size(107, 21);
+            this.buttonDest.TabIndex = 3;
+            this.buttonDest.Text = "Dest";
+            this.buttonDest.UseVisualStyleBackColor = true;
+            this.buttonDest.Click += new System.EventHandler(this.buttonDest_Click);
+            // 
+            // buttonDo
+            // 
+            this.buttonDo.Location = new System.Drawing.Point(441, 390);
+            this.buttonDo.Name = "buttonDo";
+            this.buttonDo.Size = new System.Drawing.Size(107, 21);
+            this.buttonDo.TabIndex = 4;
+            this.buttonDo.Text = "Copy";
+            this.buttonDo.UseVisualStyleBackColor = true;
+            this.buttonDo.Click += new System.EventHandler(this.buttonDo_Click);
+            // 
+            // textBoxNames
+            // 
+            this.textBoxNames.Location = new System.Drawing.Point(12, 112);
+            this.textBoxNames.Multiline = true;
+            this.textBoxNames.Name = "textBoxNames";
+            this.textBoxNames.Size = new System.Drawing.Size(407, 299);
+            this.textBoxNames.TabIndex = 5;
+            // 
+            // FileMoveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 423);
+            this.Controls.Add(this.textBoxNames);
+            this.Controls.Add(this.buttonDo);
+            this.Controls.Add(this.buttonDest);
+            this.Controls.Add(this.buttonSour);
+            this.Controls.Add(this.textBoxDest);
+            this.Controls.Add(this.textBoxSour);
+            this.Name = "FileMoveForm";
+            this.Text = "FileCopy";
+            this.Load += new System.EventHandler(this.FileMoveForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSour;
+        private System.Windows.Forms.TextBox textBoxDest;
+        private System.Windows.Forms.Button buttonSour;
+        private System.Windows.Forms.Button buttonDest;
+        private System.Windows.Forms.Button buttonDo;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogForMove;
+        private System.Windows.Forms.TextBox textBoxNames;
+    }
+}
+
---
+++ ImageAnalyzerApp/FileMoveApp/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileMoveApp
+{
+    public partial class FileMoveForm : Form
+    {
+        // Private variables
+        private string szSourDirectoryPath = string.Empty;
+        private string szDestDirectoryPath = string.Empty;
+
+        public FileMoveForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FileMoveForm_Load(object sender, EventArgs e)
+        {
+            InitData_SourDirectoryPath();
+            InitData_DestDirectoryPath();
+            RefreshUI_SourDirectoryPath();
+            RefreshUI_DestDirectoryPath();
+        }
+
+        private void buttonSour_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialogForMove.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            var result = folderBrowserDialogForMove.ShowDialog();
+            if (result != DialogResult.OK)
+                return;
+
+            if (Directory.Exists(folderBrowserDialogForMove.SelectedPath) == false)
+                return;
+
+            // Refresh Data
+            szSourDirectoryPath = folderBrowserDialogForMove.SelectedPath;
+
+            // Refresh UI
+            RefreshUI_SourDirectoryPath();
+        }
+
+        private void buttonDest_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialogForMove.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            var result = folderBrowserDialogForMove.ShowDialog();
+            if (result != DialogResult.OK)
+                return;
+
+            if (Directory.Exists(folderBrowserDialogForMove.SelectedPath) == false)
+                return;
+
+            // Refresh Data
+            szDestDirectoryPath = folderBrowserDialogForMove.SelectedPath;
+
+            // Refresh UI
+            RefreshUI_DestDirectoryPath();
+        }
+
+
+        private Dictionary<string, string> dicMoveTarget = new Dictionary<string, string>();
+
+        private List<TargetFileInfo> listTargetFile = new List<TargetFileInfo>();
+
+        struct TargetFileInfo
+        {
+            public string Name;
+            public string Path;
+        }
+
+        private void buttonDo_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(szSourDirectoryPath) ||
+                string.IsNullOrEmpty(szDestDirectoryPath))
+            {
+                MessageBox.Show("Path Empty.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(textBoxNames.Text))
+            {
+                MessageBox.Show("Name Empty.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var paths = Directory.GetFiles(szSourDirectoryPath, "*.jpg");
+            if (null == paths)
+            {
+                MessageBox.Show("Target File Empty.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var NamesOrigin = textBoxNames.Text;
+            var NamesConvert = NamesOrigin.Replace(" ", string.Empty);
+            NamesConvert = NamesConvert.Replace("\r", string.Empty);
+            var NamesArray = NamesConvert.Split('\n');
+            dicMoveTarget.Clear();
+            foreach (var Name in NamesArray)
+            {
+                if (string.IsNullOrEmpty(Name) == false)
+                {
+                    if (dicMoveTarget.ContainsKey(Name) == false)
+                        dicMoveTarget.Add(Name, Name);
+                }
+            }
+
+            if (dicMoveTarget.Count == 0)
+            {
+                MessageBox.Show("Copy Target Names Empty.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            listTargetFile.Clear();
+            for (int i = 0; i < paths.Length; ++i)
+                listTargetFile.Add(new TargetFileInfo() { Name = Path.GetFileName(paths[i]), Path = paths[i] });
+            listTargetFile.Sort((l, r) => l.Name.CompareTo(r.Name));
+
+            int Count = 0;
+            foreach (var targetInfo in listTargetFile)
+            {
+                if (dicMoveTarget.ContainsKey(targetInfo.Name))
+                {
+                    File.Copy(targetInfo.Path, szDestDirectoryPath + "\\" + targetInfo.Name);
+                    ++Count;
+                }
+            }
+
+            MessageBox.Show(string.Format("Complete. count : {0}", Count), "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void InitData_SourDirectoryPath()
+        {
+            szSourDirectoryPath = string.Empty;
+        }
+
+        private void InitData_DestDirectoryPath()
+        {
+            szDestDirectoryPath = string.Empty;
+        }
+
+        private void RefreshUI_SourDirectoryPath()
+        {
+            if (string.IsNullOrEmpty(szSourDirectoryPath) == false)
+                textBoxSour.Text = szSourDirectoryPath;
+            else
+                textBoxSour.Text = "NONE";
+        }
+
+        private void RefreshUI_DestDirectoryPath()
+        {
+            if (string.IsNullOrEmpty(szDestDirectoryPath) == false)
+                textBoxDest.Text = szDestDirectoryPath;
+            else
+                textBoxDest.Text = "NONE";
+        }
+    }
+}
